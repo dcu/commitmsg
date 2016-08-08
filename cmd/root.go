7db50a42d20219f387cdc37eae0d2d59a10a21b4
David A. Cuadrado <krawek@gmail.com>
2016-08-08 09:15:54 -0500

    Cleanup help text
    Also rename file main.go -> commitmsg.go

--- cmd/root.go
+++ cmd/root.go
@@ -37,16 +37,11 @@ var (
 
 var cfgFile string
 
-// This represents the base command when called without any subcommands
+// RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "commitmsg",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "commitmsg checks the format of the commit message",
+	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
